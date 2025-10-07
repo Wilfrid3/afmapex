83defdeeea442b8dfea1e784a5f8047f918d2982
Wilfrid3 <sieweive@gmail.com>
2025-10-08 00:09:26 +0100

    Commit Error

--- src/components/QuoteSection.jsx
+++ src/components/QuoteSection.jsx
@@ -10,7 +10,7 @@ export default function QuoteSection() {
           <div className="h-full flex flex-col justify-center">
             <div className="container mx-auto px-6 text-center text-white">
               <div className="text-right">
-                <p className="text-gray-700 text-2xl text-justify ml-auto w-[600px]">"{t('quotes.p1')}"</p>
+                <p className="text-gray-700 text-xl text-justify ml-auto w-[600px]">"{t('quotes.p1')}"</p>
                 <p className="mt-4 text-sm text-black font-semibold">{t('quotes.pl')}</p>
                 <p className="text-sm text-black italic ">{t('quotes.pp')}<br />{t('quotes.pd')}</p>
               </div>
@@ -22,7 +22,7 @@ export default function QuoteSection() {
           <div className="h-full flex flex-col justify-center">
             <div className="container mx-auto px-6 text-center text-white">
               <div className="text-right">
-                <p className="text-gray-700 text-2xl text-justify ml-auto w-[600px]">"{t('quotes.p2')}"</p>
+                <p className="text-gray-700 text-xl text-justify ml-auto w-[600px]">"{t('quotes.p2')}"</p>
                 <p className="mt-4 text-sm text-black font-semibold">{t('quotes.pm')}</p>
                 <p className="text-sm text-black italic ">{t('quotes.ppm')}<br />{t('quotes.pdm')}</p>
               </div>
@@ -34,7 +34,7 @@ export default function QuoteSection() {
           <div className="h-full flex flex-col justify-center">
             <div className="container mx-auto px-6 text-center text-white">
               <div className="text-right">
-                <p className="text-gray-700 text-2xl text-justify ml-auto w-[600px]">"{t('quotes.p3')}"</p>
+                <p className="text-gray-700 text-xl text-justify ml-auto w-[600px]">"{t('quotes.p3')}"</p>
                 <p className="mt-4 text-sm text-black font-semibold">{t('quotes.mt')}</p>
                 <p className="text-sm text-black italic ">{t('quotes.mtp')}</p>
               </div>
